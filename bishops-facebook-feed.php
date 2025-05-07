16731471a7e2bf13e43be93db6d73fb8f9f9bd04
Revelation <revnyirongo@live.com>
2025-05-07 09:38:11 +0200

    Update bishops-facebook-feed.php

--- bishops-facebook-feed.php
+++ bishops-facebook-feed.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name: Bishops Facebook Feed
  * Description: Custom Facebook feed for Malawi Conference of Catholic Bishops
- * Version: 1.0
- * Author: Your Name
+ * Version: 1.1
+ * Author: Revelation Nyirongo
  */
 
 // Exit if accessed directly
@@ -16,6 +16,9 @@ class Bishops_Facebook_Feed {
     // Plugin settings
     private $page_id;
     private $access_token;
+    private $post_count;
+    private $post_length;
+    private $click_behavior;
     
     public function __construct() {
         // Initialize plugin
@@ -35,6 +38,9 @@ class Bishops_Facebook_Feed {
         // Get settings
         $this->page_id = get_option('bishops_fb_page_id', '');
         $this->access_token = get_option('bishops_fb_access_token', '');
+        $this->post_count = get_option('bishops_fb_post_count', 3);
+        $this->post_length = get_option('bishops_fb_post_length', 100);
+        $this->click_behavior = get_option('bishops_fb_click_behavior', 'facebook');
         
         // Register scripts and styles
         add_action('wp_enqueue_scripts', array($this, 'register_scripts'));
@@ -46,7 +52,7 @@ class Bishops_Facebook_Feed {
             'bishops-facebook-feed-css', 
             plugin_dir_url(__FILE__) . 'css/facebook-feed.css',
             array(),
-            '1.0'
+            '1.1'
         );
         
         // Register custom JS
@@ -54,7 +60,7 @@ class Bishops_Facebook_Feed {
             'bishops-facebook-feed-js',
             plugin_dir_url(__FILE__) . 'js/facebook-feed.js',
             array('jquery'),
-            '1.0',
+            '1.1',
             true
         );
     }
@@ -72,6 +78,21 @@ class Bishops_Facebook_Feed {
     public function register_settings() {
         register_setting('bishops_fb_settings', 'bishops_fb_page_id');
         register_setting('bishops_fb_settings', 'bishops_fb_access_token');
+        register_setting('bishops_fb_settings', 'bishops_fb_post_count', array(
+            'type' => 'integer',
+            'sanitize_callback' => 'absint',
+            'default' => 3
+        ));
+        register_setting('bishops_fb_settings', 'bishops_fb_post_length', array(
+            'type' => 'integer',
+            'sanitize_callback' => 'absint',
+            'default' => 100
+        ));
+        register_setting('bishops_fb_settings', 'bishops_fb_click_behavior', array(
+            'type' => 'string',
+            'sanitize_callback' => 'sanitize_text_field',
+            'default' => 'facebook'
+        ));
     }
     
     public function render_settings_page() {
@@ -94,6 +115,30 @@ class Bishops_Facebook_Feed {
                             <p class="description">This should be a long-lived access token. See documentation for instructions.</p>
                         </td>
                     </tr>
+                    <tr>
+                        <th scope="row">Number of Posts</th>
+                        <td>
+                            <input type="number" name="bishops_fb_post_count" value="<?php echo esc_attr(get_option('bishops_fb_post_count', 3)); ?>" min="1" max="12" class="small-text" />
+                            <p class="description">How many posts to display (1-12)</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Post Length</th>
+                        <td>
+                            <input type="number" name="bishops_fb_post_length" value="<?php echo esc_attr(get_option('bishops_fb_post_length', 100)); ?>" min="10" max="500" class="small-text" />
+                            <p class="description">Maximum number of characters to display for each post (10-500)</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Click Behavior</th>
+                        <td>
+                            <select name="bishops_fb_click_behavior">
+                                <option value="facebook" <?php selected(get_option('bishops_fb_click_behavior', 'facebook'), 'facebook'); ?>>Open Facebook Post</option>
+                                <option value="none" <?php selected(get_option('bishops_fb_click_behavior', 'facebook'), 'none'); ?>>No Click Action</option>
+                            </select>
+                            <p class="description">What happens when a user clicks on a post</p>
+                        </td>
+                    </tr>
                 </table>
                 <?php submit_button(); ?>
             </form>
@@ -102,6 +147,13 @@ class Bishops_Facebook_Feed {
     }
     
     public function render_facebook_feed($atts) {
+        // Parse shortcode attributes
+        $atts = shortcode_atts(array(
+            'count' => $this->post_count,
+            'length' => $this->post_length,
+            'click' => $this->click_behavior
+        ), $atts);
+        
         // Enqueue required styles and scripts
         wp_enqueue_style('bishops-facebook-feed-css');
         wp_enqueue_script('bishops-facebook-feed-js');
@@ -109,7 +161,10 @@ class Bishops_Facebook_Feed {
         // Pass variables to JavaScript
         wp_localize_script('bishops-facebook-feed-js', 'bishopsFBSettings', array(
             'ajaxurl' => admin_url('admin-ajax.php'),
-            'nonce' => wp_create_nonce('bishops_fb_nonce')
+            'nonce' => wp_create_nonce('bishops_fb_nonce'),
+            'postCount' => absint($atts['count']),
+            'postLength' => absint($atts['length']),
+            'clickBehavior' => sanitize_text_field($atts['click'])
         ));
         
         // Start output buffering
@@ -138,11 +193,15 @@ class Bishops_Facebook_Feed {
         
         $page_id = $this->page_id;
         $access_token = $this->access_token;
+        $post_count = isset($_GET['post_count']) ? absint($_GET['post_count']) : $this->post_count;
+        
+        // Ensure post count is valid
+        $post_count = max(1, min(12, $post_count));
         
         // Build the API endpoint URL
         $endpoint = "https://graph.facebook.com/v19.0/{$page_id}/posts";
         $fields = "id,message,created_time,attachments{media_type,media,title,description,url},likes.summary(true),comments.summary(true)";
-        $url = "{$endpoint}?fields={$fields}&access_token={$access_token}&limit=6";
+        $url = "{$endpoint}?fields={$fields}&access_token={$access_token}&limit={$post_count}";
         
         // Make the API request
         $response = wp_remote_get($url);
